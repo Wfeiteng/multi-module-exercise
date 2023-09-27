e63ab723c340f50082e436346a1d03408b6a3728
Wfeiteng <wzt_it@163.com>
2023-09-27 22:11:30 +0800

    java8

---
+++ design-pattern/src/test/java/com/multi/module/exercise/designpattern/DesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.module.exercise.designpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
